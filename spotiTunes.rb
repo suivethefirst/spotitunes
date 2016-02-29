514477a3c0e886a4cbc6b4e8281fc4d4300505d5
David Sullivan <david.sullivan@novosco.com>
2016-02-29 15:43:45 +0000

    Add track handling and clear up formatting with some spacing

--- spotiTunes.rb
+++ spotiTunes.rb
@@ -3,25 +3,39 @@ require 'httparty'
 require 'json'
 
 def parseMessage(message)
+
 	spotifyURL = /spotify:(\balbum|\btrack):[a-zA-Z0-9]+/.match(message)
+
 	if spotifyURL.nil?
 		return "-1"
 	else
 		return spotifyURL.to_s
 	end
+
 end
 
 def getArtistAlbumFromSpotifyURL(spotifyURL)
+
 	spotifyParsed = spotifyURL.split(':')
-	url = "https://api.spotify.com/v1/albums/#{spotifyParsed[2]}"
+
+	if spotifyParsed[1] = 'album'
+		url = "https://api.spotify.com/v1/albums/#{spotifyParsed[2]}"
+	elsif spotifyParsed[1] = 'track'
+		url = "https://api.spotify.com/v1/tracks/#{spotifyParsed[2]}"
+	end
+
 	json_response = JSON.parse(HTTParty.get(url).body)
 	return json_response['artists'][0]['name'] + ' ' + json_response['name']
+
 end
 
 def getiTunesFirstCollectionView(searchTerm)
+
 	url = "https://itunes.apple.com/search?term=#{searchTerm}&country=GB"
 	json_response = JSON.parse(HTTParty.get(url))
+
 	return json_response['results'].first['collectionViewUrl']
+
 end
 
 get '/' do
