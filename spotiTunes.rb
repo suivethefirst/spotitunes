3310d5f79c623c541525b9cee20f71aac7d60568
Myles Gray <mg@mylesgray.com>
2016-04-12 20:04:18 +0100

    Maybe with a hash?

--- spotiTunes.rb
+++ spotiTunes.rb
@@ -121,7 +121,7 @@ end
 
 def getArtistAlbumFromGoogleURL(gmusicHash)
 
-	return {gmusicHash['artist']} + ' ' + {gmusicHash['album']}
+	return "#{gmusicHash['artist']} + ' ' + #{gmusicHash['album']}"
 
 end
 
